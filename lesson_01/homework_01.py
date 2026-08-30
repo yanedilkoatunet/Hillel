8ea9c1cee06cc19e12c716975e243129474bd2e4
Liza Nedilko <yanedilkoatunet@icloud>
2026-08-30 13:07:30 +0200

    Виконані зміни

---
+++ lesson_01/homework_01.py
@@ -0,0 +1,54 @@
+# task 01 == Виправте синтаксичні помилки
+print("Hello", end = " ")
+print("world!")
+
+# task 02 == Виправте синтаксичні помилки
+hello = "Hello"
+world = "world"
+if True:
+    print(hello + " " + world)
+
+# task 03 Вcтавте пропущену змінну у ф-цію print
+for letter in "Hello world!":
+    print(letter)
+
+# task 04 == Зробіть так, щоб кількість бананів була
+# завжди в чотири рази більша, ніж яблук
+apples = 2
+banana = apples * 4
+print(banana)
+
+# task 05 == виправте назви змінних
+storona_1 = 1 # або side_1
+storona_2 = 2 # або side_2
+storona_3 = 3 # або side_3
+storona_4 = 4 # або side_4
+
+# task 06 == Порахуйте периметр фігури з task 05
+peremitery = storona_1 + storona_2 + storona_3 + storona_4
+print(peremitery)
+
+# task 07
+apple_tree = 4
+pear_tree = apple_tree + 5
+plum_tree = pear_tree - 2
+all_trees = apple_tree + pear_tree + plum_tree
+print(all_trees)
+
+# task 08
+temperature = 5
+temperature = temperature - 10
+temperature = temperature + 4
+print(temperature)
+
+# task 09
+all_boys = 24
+all_girls = all_boys // 2
+today_presence = (all_girls - 2) + (all_boys - 1)
+print(today_presence)
+
+# task 10
+first_book = 8
+second_book = first_book + 2
+third_book = (first_book + second_book) / 2
+print(first_book, second_book, third_book)
