4b51d1631e7c3857e13830d2d906c0c86fbaad33
Liza Nedilko <yanedilkoatunet@icloud>
2026-09-12 21:15:29 +0200

    Виконані зміни

--- lesson_05/homework_05.py
+++ lesson_05/homework_05.py
@@ -45,4 +45,47 @@ car_data = {
   'Nissan Titan': ('silver', 2018, 5.6, 'pickup', 35000)
 }
 search_criteria = (2017, 1.6, 36000)
-print(search_criteria)
+found_cars = []
+for car, data in car_data.items():
+    if (
+        data[1] >= search_criteria[0]
+        and data[2] >= search_criteria[1]
+        and data[4] <= search_criteria[2]
+    ):
+        found_cars.append((data[4], car, data))
+found_cars.sort()
+print(found_cars[:5])
+
+# Given list of tuples (name, surname, age, profession, City location)
+# 1 - Add your new record o the beginning of the given list
+# 2 - In modified list swap elements with indexes 1 and 5 (1<->5). Print result
+# 3 - check that all people in modified list with records indexes 6, 10, 13
+#   have age >=30. Print condition check result
+
+people_records = [
+  ('John', 'Doe', 28, 'Engineer', 'New York'),
+  ('Alice', 'Smith', 35, 'Teacher', 'Los Angeles'),
+  ('Bob', 'Johnson', 45, 'Doctor', 'Chicago'),
+  ('Emily', 'Williams', 30, 'Artist', 'San Francisco'),
+  ('Michael', 'Brown', 22, 'Student', 'Seattle'),
+  ('Sophia', 'Davis', 40, 'Lawyer', 'Boston'),
+  ('David', 'Miller', 33, 'Software Developer', 'Austin'),
+  ('Olivia', 'Wilson', 27, 'Marketing Specialist', 'Denver'),
+  ('Daniel', 'Taylor', 38, 'Architect', 'Portland'),
+  ('Grace', 'Moore', 25, 'Graphic Designer', 'Miami'),
+  ('Samuel', 'Jones', 50, 'Business Consultant', 'Atlanta'),
+  ('Emma', 'Hall', 31, 'Chef', 'Dallas'),
+  ('William', 'Clark', 29, 'Financial Analyst', 'Houston'),
+  ('Ava', 'White', 42, 'Journalist', 'San Diego'),
+  ('Ethan', 'Anderson', 36, 'Product Manager', 'Phoenix')
+]
+
+people_records.insert(0, ('Yelyzaveta', 'Nedilko', 22, 'Manual QA', 'Katowice'))
+people_records[1], people_records[5] = people_records[5], people_records[1]
+print(people_records)
+
+print (
+ people_records[6][2] >= 30
+    and people_records[10][2] >= 30
+    and people_records[13][2] >= 30
+)
